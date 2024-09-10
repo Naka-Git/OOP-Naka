4bced9474a11a83f3e39d2d8fe32e0b4c5c785d1
naka <nakamanda2104@gmail.com>
2024-09-10 10:34:35 +0800

    pertemuan 1 PBO

--- belajar1.py
+++ belajar1.py
@@ -1,1 +1,21 @@
-print ("hello world")
+def tambah(x, y):
+    z = x + y
+    return z
+u  = 10 #ini adalah main program
+def kurang(x, y):
+    z = x - y
+    return z
+
+if 10 > 0 :
+    print('10 lebih besar dari 0') #ini disebut body if
+else:
+    print('10 lebih kecil dari 0') #ini disebut body else
+
+phi = 3.14 #main program
+
+for i in range(1,5):
+    print(i)
+
+cita_cita = ["Pilot","Dokter","Influencer"]
+for ct in cita_cita:
+    print (ct) 
